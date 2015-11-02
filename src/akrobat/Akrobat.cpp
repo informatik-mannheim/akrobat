1997f415ac4f2146b5e664c1382eb58123f95e40
Robert Kettler <kettler.robert@gmail.com>
2015-11-02 17:18:27 +0100

    refactored includes

--- src/akrobat/Akrobat.cpp
+++ src/akrobat/Akrobat.cpp
@@ -1,16 +1,19 @@
 #include <akrobat/Akrobat.h>
-#include <akrobat/akrobat_init.h>
+
 #include <cmath>
 #include <fstream>
 #include <iostream>
+
 #include <ros/ros.h>
-#include <std_msgs/Float64.h>
-#include <sensor_msgs/Joy.h>
+#include <angles/angles.h>
 #include <tf/tf.h>
 #include <tf/transform_datatypes.h>
-#include <angles/angles.h>
-#include <akrobat/akrobat_init.h>
+
+#include <std_msgs/Float64.h>
+#include <sensor_msgs/Joy.h>
 #include <sensor_msgs/JointState.h>
+
+#include <akrobat/akrobat_init.h>
 
 using namespace std;
 using namespace tf;
