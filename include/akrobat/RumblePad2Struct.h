f2fce16d2aa00f0c4efd5a07318ffebaeca36de2
Robert Kettler <kettler.robert@gmail.com>
2015-11-02 21:32:35 +0100

    changed tf test

--- include/akrobat/RumblePad2Struct.h
+++ include/akrobat/RumblePad2Struct.h
@@ -1,17 +1,4 @@
 #ifndef RUMBLEPAD2STRUCT_H
 #define RUMBLEPAD2STRUCT_H
 
-#include <ros/ros.h>
-
-#include <tf/tf.h>
-#include <tf/transform_datatypes.h>
-
-//joypad
-struct rumblePad2Struct
-{
-	tf::Vector3 speed; // forward/backward/sideward movement
-	tf::Vector3 bdR;   // body rotation
-	tf::Vector3 bdT;   // body translation
-};
-
 #endif // RUMBLEPAD2STRUCT_H
--- include/akrobat/akrobat_init.h
+++ include/akrobat/akrobat_init.h
@@ -87,4 +87,16 @@
 #define FemurJointLimit ((LCS.leg[legNum].jointAngles.beta <=maxFemur[legNum])&&(LCS.leg[legNum].jointAngles.beta >=minFemur[legNum]))
 #define TibiaJointLimit ((LCS.leg[legNum].jointAngles.gamma<=maxTibia[legNum])&&(LCS.leg[legNum].jointAngles.gamma>=minTibia[legNum]))
 
+#include <ros/ros.h>
+
+#include <tf/transform_datatypes.h>
+#include <tf/tf.h>
+
+struct rumblePad2Struct
+{
+	tf::Vector3 speed; // forward/backward/sideward movement
+	tf::Vector3 bdR;   // body rotation
+	tf::Vector3 bdT;   // body translation
+};
+
 #endif
