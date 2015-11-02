031e8ccf40d7a8dba847b759412a2e558f7f6eb2
Robert Kettler <kettler.robert@gmail.com>
2015-11-02 21:41:02 +0100

    include test

--- include/akrobat/Globals.h
+++ include/akrobat/Globals.h
@@ -1,6 +1,19 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <ros/ros.h>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <std_msgs/Float64.h>
+#include <sensor_msgs/Joy.h>
+#include <dynamixel_msgs/MotorStateList.h>
+#include <tf/tf.h>
+#include <tf/transform_datatypes.h>
+#include <angles/angles.h>
+#include <akrobat/akrobat_init.h>
+#include <sensor_msgs/JointState.h>
+
 #include <sensor_msgs/JointState.h>
 
 #include <akrobat/akrobat_init.h>
