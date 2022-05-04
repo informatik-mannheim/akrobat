1de32f09fb8bd28cfc6d4a1ee5f897f0037995fc
Karl Franke <karl.franke@outlook.de>
2022-05-04 12:24:20 +0200

    Add navigation stack

--- src/akrobat/move_base_to_akrobat.cpp
+++ src/akrobat/move_base_to_akrobat.cpp
@@ -1,10 +1,31 @@
+#include "ros/ros.h"
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <vector>
+#include "akrobat/movement.h"
+#include "std_msgs/Int64.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/Joy.h"
+#include "geometry_msgs/Twist.h"
+
+class Translater
+{   
+    public:
+    void translate_linear(geometry_msgs::Twist msg)
+	{
+		std::cout<<msg<<std::endl;
+	}
+};
+
+
+
 int main(int argc, char **argv)
 {
    ros::init(argc, argv, "move_base_to_akrobat");
    ros::NodeHandle n;
-	
-   ros::Subscriber joySub = n.subscribe<sensor_msgs::Joy>("joy", 1000, &Listener::readJoypadCallback, &l);
-	 ros::Subscriber movSub = n.subscribe<akrobat::movement>("cmd_vel", 1000, &Listener::readMovementCallback, &l);
+   Translater t;	
+   ros::Subscriber movSub = n.subscribe<geometry_msgs::Twist>("cmd_vel", 1000, &Translater::translate_linear, &t);
    ros::Publisher movPub = n.advertise<akrobat::movement>("movements", 1000);
    ros::Rate loop_rate(5);
 }	
