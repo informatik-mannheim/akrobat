ca813af92045476b240b0726e5b5594c0c1b8d40
Karl Franke <karl.franke@outlook.de>
2022-07-22 10:07:16 +0200

    Update

--- src/akrobat/TranslateJoypad.cpp
+++ src/akrobat/TranslateJoypad.cpp
@@ -9,7 +9,7 @@
 #include "sensor_msgs/Joy.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Pose.h"
-#include "std_msgs/bool.h"
+#include "std_msgs/Bool.h"
 
 float int_max = 32767.0f;
 float joystickDeadZone = 0.1f;
