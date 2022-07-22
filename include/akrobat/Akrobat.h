7d9172464cea325745256fcfc9aea3d82422b15b
Karl Franke <karl.franke@outlook.de>
2022-07-22 10:19:04 +0200

    Update

--- include/akrobat/Akrobat.h
+++ include/akrobat/Akrobat.h
@@ -112,7 +112,7 @@ public:
 	// Startup Akrobat from bottom
 	void startAkrobat();
 
-	void shutdownAkrobat(const std_msgs::Bool Shutdown);
+	void shutdownAkrobat(const std_msgs::Bool::ConstPtr& Shutdown);
 	
 	// initialize akrobat leg position
 	void initAkrobat();
