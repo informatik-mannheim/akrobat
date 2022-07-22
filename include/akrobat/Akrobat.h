a80bbe6fbe8bae0c6afa2a286c3cce50cb28f96d
Karl Franke <karl.franke@outlook.de>
2022-07-22 11:58:35 +0200

    Update

--- include/akrobat/Akrobat.h
+++ include/akrobat/Akrobat.h
@@ -154,7 +154,7 @@ public:
 	void callRumblePad2Back(const akrobat::movement::ConstPtr& mov);
 
 	float alpha;
-	double c;shutdown
+	double c;
 	float twist_movment_x;
 	float twist_movment_y;
 	float twist_movment_z;
