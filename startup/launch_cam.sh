526bfc1186f91b2a3e4828f29317d80c68e2a777
Karl Franke <karl.franke@outlook.de>
2022-07-27 11:30:36 +0200

    Update

--- startup/launch_cam.sh
+++ startup/launch_cam.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Launch the robot
-source /opt/ros/noetic/setup.bash 
-source /home/user/catkin_ws/devel/setup.bash 
+#source /opt/ros/noetic/setup.bash 
+#source /home/user/catkin_ws/devel/setup.bash 
 
-echo "Launching Robot, please wait!"
+echo "Launching Cam, please wait!"
+ssh pi@10.42.0.2
 until roslaunch akrobat_cam Akrobat_Master_Mapping.launch
 do 
     echo "Shutdown"
--- startup/launch_main.sh
+++ startup/launch_main.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Launch the robot
-source /opt/ros/noetic/setup.bash 
-source /home/user/catkin_ws/devel/setup.bash 
+#source /opt/ros/noetic/setup.bash 
+#source /home/user/catkin_ws/devel/setup.bash 
 
 echo "Launching Robot, please wait!"
+
 until roslaunch akrobat AkrobatMaster.launch
 do 
     echo "Shutdown"
