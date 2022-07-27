4a4392c8d5617bba6e4eff12f4bd926ec385551d
Karl Franke <karl.franke@outlook.de>
2022-07-27 11:54:42 +0200

    Update

--- startup/launch_cam.sh
+++ startup/launch_cam.sh
@@ -5,8 +5,8 @@
 #source /home/user/catkin_ws/devel/setup.bash 
 
 echo "Launching Cam, please wait!"
-ssh pi@10.42.0.2
-until roslaunch akrobat_cam Akrobat_Master_Mapping.launch
+
+until ssh pi@10.42.0.2 "roslaunch akrobat_cam Akrobat_Master_Mapping.launch"
 do 
     echo "Shutdown"
     sleep 2
