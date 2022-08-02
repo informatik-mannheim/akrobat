87123160276a8d1634e3a9185dd600f7ebdf9722
Karl Franke <karl.franke@outlook.de>
2022-08-02 09:55:21 +0200

    Update

--- startup/launch_main.sh
+++ startup/launch_main.sh
@@ -5,6 +5,10 @@ source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash 
 
 echo "Launching Robot, please wait!"
+while ! timeout 0.2 ping -c 1 -n 10.42.0.1 &> /dev/null
+do
+    printf "%c" "."
+done
 
 until roslaunch akrobat AkrobatMaster.launch
 
