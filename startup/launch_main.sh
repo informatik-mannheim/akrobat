e3c956f1c1cbaa7b75c76aaa246442d0e1a5150d
Karl Franke <karl.franke@outlook.de>
2022-08-02 10:55:58 +0200

    Update

--- startup/launch_main.sh
+++ startup/launch_main.sh
@@ -5,12 +5,12 @@ source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash 
 
 echo "Launching Robot, please wait!"
-while ! timeout 0.2 ping -c 1 -n 10.42.0.1 &> /dev/null
+while ! timeout 0.2 ping -c 1 -n 10.42.0.2 &> /dev/null
 do
     printf "%c" "."
 done
 
 
-gnome-terminal -e 'sh -c "~/catkin_ws/src/akrobat/startup/launch_master; exec bash"'
+gnome-terminal -e 'sh -c "~/catkin_ws/src/akrobat/startup/launch_master.sh; exec bash"'
 
 
